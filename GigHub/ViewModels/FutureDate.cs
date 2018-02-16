32efc1e5fe47066fdf81b2fdef1afd605651b05c
Bartek <bartek.iskrzycki@gmail.com>
2018-02-16 13:38:08 +0100

    implement custom validation

---
+++ GigHub/ViewModels/FutureDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                                    "d MMM yyyy", 
+                                    new CultureInfo("en-US"), 
+                                    DateTimeStyles.None, 
+                                    out var dateTime);
+
+            return isValid && dateTime > DateTime.Now;
+        }
+    }
+}
---
+++ GigHub/ViewModels/ValidTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.ViewModels
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm", 
+                new CultureInfo("en-US"), 
+                DateTimeStyles.None, 
+                out var dateTime);
+
+            return isValid;
+        }
+    }
+}

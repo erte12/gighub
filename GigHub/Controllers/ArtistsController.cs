d701457029ff3255cca8a20921bcdfb46e53e95b
Bartek <bartek.iskrzycki@gmail.com>
2018-03-02 12:52:37 +0100

    Implement view Artists I'm Following

---
+++ GigHub/Controllers/ArtistsController.cs
@@ -0,0 +1,28 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Followees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Artist).ToList();
+            
+            return View(artists);
+        }
+    }
+}
